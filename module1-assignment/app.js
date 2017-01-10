576f38c6fe650676e7c233c0c1920fca2c39d584
Cesar Tarrillo Celada <cesartarrillocelada@gmail.com>
2017-01-09 23:15:55 -0500

    add comments to improve understanding

--- module1-assignment/app.js
+++ module1-assignment/app.js
@@ -1,12 +1,20 @@
-(function () {
+/*
+ * I am adding comments as much as possible just for learning purposes
+ */
+
+(function () { // We use IIFE to make sure no local variables bleed into the global scope
     'use strict';
 
-    angular.module('LunchCheck', [])
+    angular.module('LunchCheck', []) // []: list of dependencies for Angular
         .controller('LunchCheckController', LunchCheckController);
+        //.controller('LunchCheckController', ['$scope', LunchCheckController])
 
+    // As per DI, the client is not responsable for instantiating, Angular IoC does.
+    // $inject property specifies which services to inject into which param of the Controller function
     LunchCheckController.$inject = ['$scope'];
 
-    function LunchCheckController($scope) {
+    // variables that start with $ are specially reserved for Angular
+    function LunchCheckController($scope) { 
         $scope.menu = '';
 
         $scope.checkIfTooMuch = function () {
